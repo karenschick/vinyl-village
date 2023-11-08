d51a91958629fbd5ee5894ad29d51cf5cb3c8dce
Karen Schick <karen.schick6@gmail.com>
2023-11-08 12:35:40 -0500

    albumDuration works woot woot

--- packages/client/src/components/displayAlbums.jsx
+++ packages/client/src/components/displayAlbums.jsx
@@ -3,10 +3,12 @@ import { Card, Button, ListGroup, CardTitle } from "react-bootstrap";
 import { useApiFetch } from "../util/api";
 
 export const DisplayAlbums = () => {
-  const {response} = useApiFetch("/albums")
-  console.log(response)
+  const { response } = useApiFetch("/albums");
+  console.log(response);
 
-  const handleRemoveAlbum = () => {};
+  // const handleRemoveAlbum = (albums, albumId) => {
+  //   return albums.filter((album) => album.albumId !== albumId);
+  // };
 
   const durationConversion = (duration) => {
     const minutes = Math.floor(duration / 60);
@@ -19,35 +21,46 @@ export const DisplayAlbums = () => {
   };
 
   const albumDuration = (album) => {
-    const totalDuration = album.tracks.reduce((total, track) => {      
-      return total + track.duration;
+    const totalDuration = album.tracks.reduce((total, track) => {
+      return total + parseInt(track.trackDuration, 10);
     }, 0);
-    //durationConversion
     return durationConversion(totalDuration);
   };
 
   // {albumDuration(album)}
-  
 
   return (
     <Card>
-      {response && response.map((album) => (
-        <div key={album.albumId}>
-          <Card.Header>{album.albumTitle}</Card.Header>
-          <Card.Title>{album.artistName}  ({album.bandMembers.map((member, index) => (
-            <span key={index}>
-              {member.memberName}
-              {index !== album.bandMembers.length - 1 ? ', ' : ''}
-            </span>
-          ))}) </Card.Title>
-        <Card.Subtitle>Album Duration {albumDuration(album)}</Card.Subtitle>
-        <ListGroup > 
-          {album.tracks.map((track, index)=> (<ListGroup.Item key={index}>
-            {track.trackNumber}. {track.trackTitle} - {durationConversion(track.trackDuration)}
-          </ListGroup.Item>))}
-        </ListGroup>
-        </div>
-      ))}
+      {response &&
+        response.map((album) => (
+          <div key={album.albumId}>
+            <Card.Header>{album.albumTitle}</Card.Header>
+            <Card.Title>
+              {album.artistName} (
+              {album.bandMembers.map((member, index) => (
+                <span key={index}>
+                  {member.memberName}
+                  {index !== album.bandMembers.length - 1 ? ", " : ""}
+                </span>
+              ))}
+              ){" "}
+            </Card.Title>
+            <Card.Subtitle>
+              Album Duration: {albumDuration(album)}
+            </Card.Subtitle>
+            <ListGroup>
+              {album.tracks.map((track, index) => (
+                <ListGroup.Item key={index}>
+                  {track.trackNumber}. {track.trackTitle} -{" "}
+                  {durationConversion(track.trackDuration)}
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+            {/* <Button onClick={handleRemoveAlbum(album.album, album.albumId)}>
+              remove
+            </Button> */}
+          </div>
+        ))}
     </Card>
   );
 };

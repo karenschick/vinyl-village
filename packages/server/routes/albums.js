7c851e373ddde9c25f157b800577f410dda06be1
Karen Schick <karen.schick6@gmail.com>
2023-12-30 16:47:13 -0500

    prettier

--- packages/server/routes/albums.js
+++ packages/server/routes/albums.js
@@ -5,8 +5,7 @@ import Album from "../models/album";
 const albumRouter = express.Router();
 
 albumRouter.get("/albums", async (req, res) => {
-  
-  const sortAlbum = req.query.sortBy || "albumTitle" 
+  const sortAlbum = req.query.sortBy || "albumTitle";
   try {
     const albums = await Album.find().sort({ [sortAlbum]: 1 });
     res.json(albums);
@@ -24,21 +23,21 @@ albumRouter.post("/albums", async (req, res) => {
     res.status(201).json(newAlbum);
   } catch (error) {
     console.error(error);
-    if (error.name === 'ValidationError') {
+    if (error.name === "ValidationError") {
       return res.status(400).json({ error: error.message });
     }
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-albumRouter.delete("/albums/:id", async (req, res)=>{
-  try{
-    await Album.findByIdAndRemove(req.params.id)
-    res.status(204).send()
-  } catch(error){
-    console.error(error)
-    res.status(500).json({error: "Internal Server Error"})
+albumRouter.delete("/albums/:id", async (req, res) => {
+  try {
+    await Album.findByIdAndRemove(req.params.id);
+    res.status(204).send();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
-})
+});
 
 export default albumRouter;

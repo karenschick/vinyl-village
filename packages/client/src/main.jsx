5cd34d231258122dffd24e3ad5237fe925f8e307
Karen Schick <karen.schick6@gmail.com>
2024-01-14 14:34:46 -0500

    error boundary

--- packages/client/src/main.jsx
+++ packages/client/src/main.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import ErrorBoundary from "./components/ErrorBoundary";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   
     <BrowserRouter>
+    <ErrorBoundary>
       <Routes>
+        
         <Route exact path="" element={<HomePage />} />
+        
         {/* Add more routes here */}
       </Routes>
+      </ErrorBoundary>
     </BrowserRouter>
   
 );

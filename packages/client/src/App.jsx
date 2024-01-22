c0c48da3b23e2af6bfa3e94d7cd7b6a7197bde33
Karen Schick <karen.schick6@gmail.com>
2024-01-22 15:09:44 -0500

    building app and main

---
+++ packages/client/src/App.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+import { Container, Row, Col } from "react-bootstrap";
+
+
+
+
+const App = () => {
+  const {
+    state: { user },
+  } = useProvideAuth();
+
+  return (
+    <ErrorBoundary>
+      <ToastContainer position="top-center" />
+      {user ? (
+        <>
+          <Header />
+          <Container
+            fluid
+            style={{
+              height: "calc(100vh - 72px)",
+              overflow: "auto",
+            }}
+          >
+            <Row>
+              <Col xs={0} md={2} xl={3} />
+              <Col xs={12} md={8} xl={6}>
+                <Routes>
+                  {/* <Route exact path="/u/:uname" element={<UserDetailPage />} />
+                  <Route exact path="/p/:pid" element={<PostDetailPage />} />
+                  <Route exact path="/" element={<Feed />} />
+                  <Route exact path="/login" element={<Navigate to="/" />} /> */}
+                  {/* <Route exact path="/register" element={<Navigate to="/" />} /> */}
+                  {/* <Route
+                    element={({ location }) => {
+                      return (
+                        <div
+                          style={{
+                            padding: "50px",
+                            width: "100%",
+                            textAlign: "center",
+                          }}
+                        >
+                          The page <code>{location.pathname}</code> could not be
+                          found.
+                        </div>
+                      );
+                    }}
+                  /> */}
+                </Routes>
+              </Col>
+              <Col xs={0} md={2} xl={3} />
+            </Row>
+          </Container>
+        </>
+      ) : (
+        <Routes>
+          {/* <Route path="/login" element={<LoginPage />} /> */}
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      )}
+    </ErrorBoundary>
+  );
+};
+
+export default App;
--- packages/client/src/main.jsx
+++ packages/client/src/main.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import ErrorBoundary from "./components/ErrorBoundary";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
+import { ProvideAuth } from "/hooks/useAuth";
+import App from "./App";
+//import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <ErrorBoundary>
-      <Routes>
-        <Route exact path="" element={<HomePage />} />
-
-        {/* Add more routes here */}
-      </Routes>
-    </ErrorBoundary>
-  </BrowserRouter>
+  <React.StrictMode>
+    <ProvideAuth>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ProvideAuth>
+  </React.StrictMode>
 );

9b68c88d6c5e073f9c7affbbbd3ed954d180a24d
Karen Schick <karen.schick6@gmail.com>
2024-01-24 11:32:33 -0500

    added username

--- packages/client/src/pages/HomePage.jsx
+++ packages/client/src/pages/HomePage.jsx
@@ -1,13 +1,24 @@
 import { useApiFetch } from "../util/api";
 import DisplayAlbums from "../components/displayAlbums";
+import { useProvideAuth } from "../hooks/useAuth";
+import { useRequireAuth } from "../hooks/useRequireAuth";
 import LoadingSpinner from "../components/LoadingSpinner/LoadingSpinner";
 
 export default function HomePage(props) {
   const { error, isLoading, response } = useApiFetch("/albums");
+  const { state } = useProvideAuth();
+  const {
+    state: { user, isAuthenticated },
+  } = useRequireAuth();
 
+  function capitalizeFirstLetter(string) {
+    if (!string) return '';
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  
   return (
     <main>
-      <h1 className="mt-5">Album Collection</h1>
+      <h1 className="mt-5">{capitalizeFirstLetter(state.user?.username)}'s Album Collection</h1>
       {error && (
         <h3 style={{ color: "red" }}>
           Error Loading Data: {JSON.stringify(error)}

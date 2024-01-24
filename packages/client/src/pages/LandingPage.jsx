e096b5b0ae6e36fa0ed044bc6945516792ddeb97
Karen Schick <karen.schick6@gmail.com>
2024-01-24 11:48:11 -0500

    working on landing

--- packages/client/src/pages/LandingPage.jsx
+++ packages/client/src/pages/LandingPage.jsx
@@ -7,7 +7,7 @@ import { Container } from 'react-bootstrap';
 const LandingPage = () => {
   return (
     <Container>
-      
+      <h1>Need an album? Share an album. chat with other music lovers</h1>
       
       <Login />
     </Container>

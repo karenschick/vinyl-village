8a2a8e5e72a1593a9e9ac61ee8750d4138e6f256
Karen Schick <karen.schick6@gmail.com>
2024-02-10 23:33:30 -0500

    updated post styling

--- packages/client/src/components/Post/Post.jsx
+++ packages/client/src/components/Post/Post.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import {
   Container,
+  Row,
+  Col,
   Button,
   Card,
   Figure,
@@ -13,10 +15,6 @@ import { useNavigate, Link } from "react-router-dom";
 import { toast } from "react-toastify";
 import "./Post.scss";
 import DeleteModal from "../DeleteModal/DeleteModal.jsx";
-import TrashIcon from "../icons/TrashIcon.jsx";
-import LikeIcon from "../icons/LikeIcon.jsx";
-import ReplyIcon from "../icons/ReplyIcon.jsx";
-import LikeIconFill from "../icons/LikeIconFill.jsx";
 import { useProvideAuth } from "../../hooks/useAuth";
 import api from "../../util/api.jsx";
 import { timeSince } from "../../util/timeSince";
@@ -61,14 +59,14 @@ const Post = ({
   };
 
   const handleCancelEdit = () => {
-    setEditedText(text); // Reset text to original if cancelled
+    setEditedText(text);
     setEditMode(false);
   };
 
   const toggleEditMode = () => {
     setEditMode(!editMode);
     if (!editMode) {
-      setEditedText(text); // Reset to original text when entering edit mode
+      setEditedText(text);
     }
   };
 
@@ -119,130 +117,152 @@ const Post = ({
   return (
     <>
       <ListGroup.Item className="text-danger rounded-edge" as={"div"}>
-        <Card className=" py-2 px-3 d-flex flex-row gap-3 align-items-start78 mt-2">
-          <Figure
-            as={Link}
-            to={`/u/${author.username}`}
-            className="mr-4 bg-border-color rounded-circle ml-2 p-1"
-            style={{
-              height: "70px",
-              minHeight: "70px",
-              width: "70px",
-              minWidth: "70px",
-              marginTop: "0px",
-            }}
-          >
-            <Figure.Image
-              src={author.profile_image}
-              className="avatar w-100 h-100 mr-4"
-            />
-          </Figure>
-          <div className="w-100">
-            <div className="d-flex align-items-center">
-              <Link
+        <Card
+          className={`p-2 mt-2 ${
+            user.username === author.username ? "pt-1" : "pt-4"
+          }`}
+        >
+          <div className="d-flex justify-content-end align-items-center ">
+            {user.username === author.username && (
+              <Dropdown>
+                <Dropdown.Toggle
+                  className="d-flex align-items-center"
+                  variant="light"
+                  id="dropdown-basic"
+                  bsPrefix="p-0"
+                  style={{ lineHeight: ".75", padding: "0", fontSize: "30px" }}
+                >
+                  <span className="text-muted">&#8230;</span>
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item onClick={toggleEditMode}>Edit</Dropdown.Item>
+                  <Dropdown.Item onClick={toggleShowDelete}>
+                    Delete
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            )}
+          </div>
+          <Row className="d-flex justify-content-md-start align-items-center ">
+            <Col className="d-flex flex-column align-items-center">
+              <Figure
+                as={Link}
                 to={`/u/${author.username}`}
-                className="text-muted mr-1 username"
-                variant="secondary"
+                className="mr-4 bg-border-color ml-2 p-1  "
+                style={{
+                  height: "70px",
+                  minHeight: "70px",
+                  width: "70px",
+                  minWidth: "70px",
+                  marginTop: "0px",
+                  textDecoration: "none",
+                }}
               >
-                {author.username}
-              </Link>
-              <pre className="m-0 text-muted">{" - "}</pre>
-              <span className="text-muted">{timeSince(created)} ago</span>
-              <OverlayTrigger
+                <Figure.Image
+                  src={author.profile_image}
+                  className="avatar w-100 h-100 mr-4"
+                />
+              </Figure>
+            </Col>
+            <Col md={10}>
+              <div className="d-flex align-items-center justify-content-start">
+                <div className="username">
+                  <Link
+                    to={`/u/${author.username}`}
+                    className=" mr-1"
+                    style={{ textDecoration: "none" }}
+                  >
+                    {author.username}
+                    {" - "}
+                  </Link>
+                </div>
+                <span className="">
+                  {" - "}
+                  {` ${timeSince(created)} ago`}
+                </span>
+              </div>
+              <div className="d-flex align-items-center justify-content-start mt-2">
+                <OverlayTrigger
                   overlay={
                     <Tooltip id="tooltip">
                       {likes.map((like) => like.username).join(", ")}
                     </Tooltip>
                   }
                 >
-                  <div> likes: {likesState}</div> 
-                </OverlayTrigger> <Button
-                  variant="link"
-                  size="md"
-                  onClick={() => navigate(`/p/${_id}`)}
-                > Comments:
-                  
-                </Button>
-                <div> {comments.length > 0 ? comments.length : 0}</div>
-            </div>
-            <div className="mb-n1 mt-1 position-relative">
-              <div className="mb-n1 mt-1 position-relative">
-                {editMode ? (
-                  <div>
-                    <textarea
-                      className="p-2"
-                      value={editedText}
-                      onChange={handleTextChange}
-                    />
-                    <div>
-                      <Button variant="success" onClick={handleSaveEdit}>
-                        Save
-                      </Button>
-                      <Button variant="secondary" onClick={handleCancelEdit}>
-                        Cancel
-                      </Button>
-                    </div>
-                  </div>
-                ) : (
-                  <blockquote className="mb-1 mw-100">
-                    <div className="mw-100 overflow-hidden mt-2">{text}</div>
-                  </blockquote>
-                )}
-                {image && image !== defaultImage && (
-                  <img
-                    src={image}
-                    alt="Post"
-                    style={{ maxWidth: "300px", maxheight: "auto" }}
-                  />
-                )}
-              </div>
-            </div>
-
-            <div className="d-flex justify-content-end align-items-to">
-              {user.username === author.username && (
-                <Dropdown>
-                  <Dropdown.Toggle
-                    variant="success"
-                    id="dropdown-basic"
-                    bsPrefix="p-0"
-                  >
-                    <span className="text-muted" style={{ fontSize: "30px" }}>
-                      &#8230;
-                    </span>
-                  </Dropdown.Toggle>
-
-                  <Dropdown.Menu>
-                    <Dropdown.Item onClick={toggleEditMode}>Edit</Dropdown.Item>
-                    <Dropdown.Item onClick={toggleShowDelete}>
-                      Delete
-                    </Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              )}
-
-              <div className="d-flex align-items-center mr-2">
+                  <span>
+                    {likesState === 1 ? "1 like" : `${likesState} likes`}
+                  </span>
+                </OverlayTrigger>
                 <Button
+                  style={{ textDecoration: "none" }}
                   variant="link"
                   size="md"
+                  className="ml-2"
                   onClick={() => navigate(`/p/${_id}`)}
-                > Comment
-                  
+                >
+                  {comments.length === 1
+                    ? "1 comment"
+                    : `${comments.length} comments`}
                 </Button>
-                
               </div>
-              <div
-                className={`d-flex align-items-center mr-3 ${
-                  likedState ? "isLiked" : ""
-                }`}
+            </Col>
+          </Row>
+
+          <Row className="mt-5 text-center">
+            {image && image !== defaultImage && (
+              <img
+                src={image}
+                alt="Post"
+                style={{ maxWidth: "300px", maxheight: "auto", margin: "auto" }}
+              />
+            )}
+            {editMode ? (
+              <div>
+                <textarea
+                  className="p-2"
+                  value={editedText}
+                  onChange={handleTextChange}
+                />
+                <div>
+                  <Button variant="success" onClick={handleSaveEdit}>
+                    Save
+                  </Button>
+                  <Button variant="secondary" onClick={handleCancelEdit}>
+                    Cancel
+                  </Button>
+                </div>
+              </div>
+            ) : (
+              <blockquote className="mb-1 mw-100">
+                <div className="mw-100 overflow-hidden mt-2">{text}</div>
+              </blockquote>
+            )}
+          </Row>
+          <Row className="mt-5">
+            <div className="text-center">
+              <Button
+                className="m-2"
+                variant="info"
+                style={{ border: "none", color: "white" }}
+                size="md"
+                onClick={() => navigate(`/p/${_id}`)}
               >
-                <Button variant="link" size="md" onClick={handleToggleLike}>
-                  Like
-                </Button>
-                
-                
-              </div>
+                {" "}
+                Comment
+              </Button>
+
+              <Button
+                className="m-2"
+                variant="info"
+                style={{ border: "none", color: "white" }}
+                size="md"
+                onClick={handleToggleLike}
+              >
+                Like
+              </Button>
             </div>
-          </div>
+          </Row>
         </Card>
       </ListGroup.Item>
 

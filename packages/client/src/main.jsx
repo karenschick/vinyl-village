c1cc88441ba08bf55ab2bce90746f01f798033c2
Karen Schick <karen.schick6@gmail.com>
2024-03-04 10:21:25 -0500

    loading spinner landing page allow for mongodb connection

--- packages/client/src/main.jsx
+++ packages/client/src/main.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ProvideAuth } from "./hooks/useAuth";
 import App from "./App";
+import LoadingSpinner from "./components/LoadingSpinner/LoadingSpinner";
 
+const Root = () => {
+  const [loading, setLoading] = useState(true);
 
+  //simulate MongoDB connection
+  useEffect(() => {
+    const time = setTimeout(() => {
+      setLoading(false);
+    }, 8000);
+    return () => clearTimeout(timer);
+  }, []);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <ProvideAuth>
-      <App />
-    </ProvideAuth>
-  </BrowserRouter>
-);
+  return (
+    <BrowserRouter>
+      <ProvideAuth>
+        {loading ? <LoadingSpinner full={true} /> : <App />}
+      </ProvideAuth>
+    </BrowserRouter>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Root />);

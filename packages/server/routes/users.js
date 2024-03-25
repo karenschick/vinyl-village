db3e9784c9b2a16f8111afd08cff1fa25424e8c3
Karen Schick <karen.schick6@gmail.com>
2024-03-25 12:36:24 -0400

    added conditional check for no user found based on username

--- packages/server/routes/users.js
+++ packages/server/routes/users.js
@@ -14,58 +14,13 @@ router
       populate: { path: "author", select: ["username", "profile_image"] },
     };
     const user = await User.findOne({ username }).populate(populateQuery);
+    if (!user) {
+      return res.status(404).json({ error: "user not found" });
+    }
     res.json(user.toJSON());
   })
 
   .put(requireAuth, async (req, res) => {
-    // const { password, currentPassword, confirmPassword, firstName, lastName, city, state } = req.body;
-    // const { username } = req.params;
-
-    // //console.log(req.body);
-
-    // if (password.length < 8 || password.length > 20 || confirmPassword.length < 8 || confirmPassword.length > 20) {
-    //   return res.status(422).json({
-    //     error: "must be 8-20 characters",
-    //   });
-    // }
-
-    // if (password !== confirmPassword) {
-    //   return res.status(422).json({
-    //     error: " passwords do not match",
-    //   });
-    // }
-
-    // const user = await User.findOne({ username: username });
-    // const passwordCorrect =
-    //   user === null
-    //     ? false
-    //     : await bcrypt.compare(currentPassword, user.passwordHash);
-
-    // if (!(user && passwordCorrect)) {
-    //   return res.status(401).json({
-    //     error: "invalid username or password",
-    //   });
-    // }
-
-    // const hashedpassword = await bcrypt.hash(password, 12);
-
-    // try {
-    //   const userUpdate = await User.findOneAndUpdate(
-    //     {
-    //       username,
-    //     },
-    //     {
-    //       passwordHash: hashedpassword,
-    //     },
-    //     {
-    //       new: true,
-    //     }
-    //   );
-
-    //   res.json(userUpdate.toJSON());
-    // } catch (error) {
-    //   res.status(404).end();
-    // }
     const {
       password,
       currentPassword,

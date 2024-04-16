3a3b2c2512b037dc23dd7d4a4534e0b6864f6e0f
Karen Schick <karen.schick6@gmail.com>
2024-04-16 10:56:39 -0400

    made capitalizefirstletter an util file

---
+++ packages/client/src/util/capitalizeFirstLetter.js
@@ -0,0 +1,8 @@
+export const capitalizeFirstLetter = (string) => {
+  if (!string) return "Unknown";
+  return string
+    .toLowerCase()
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+};

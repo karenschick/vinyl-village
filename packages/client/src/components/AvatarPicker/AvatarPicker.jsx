049024f57951c9416ee2053eec86538d22b27ce3
Karen Schick <karen.schick6@gmail.com>
2024-10-17 16:04:53 -0400

    removed all instances of uploadprofilephoto

--- packages/client/src/components/AvatarPicker/AvatarPicker.jsx
+++ packages/client/src/components/AvatarPicker/AvatarPicker.jsx
@@ -1,7 +1,6 @@
 // Importing necessary components from React and react-bootstrap
 //import { Container, Image } from "react-bootstrap"; // Container for layout, Image for displaying avatars
 import "./AvatarPicker.scss"; // Importing custom styles for AvatarPicker component
-//import UploadProfilePhoto from "../UploadProfilePhoto/UploadProfilePhoto";
 import React, { useState, useEffect } from "react";
 import { Image, Card, Button, Container } from "react-bootstrap";
 import { useParams } from "react-router-dom";

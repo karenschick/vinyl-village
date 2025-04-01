1cec8addcca093ac137c781483ec3bd6af72ea75
Karen Schick <karen.schick6@gmail.com>
2025-04-01 09:45:31 -0400

    newtoken

--- packages/server/config/keys.js
+++ packages/server/config/keys.js
@@ -1,27 +1,29 @@
 // Import the "git-repo-name" package to retrieve the name of the Git repository
-let gitRepoName = require("git-repo-name");
+//let gitRepoName = require("git-repo-name");
 // Get the repository name synchronously from two directory levels above the current file
-let repoName = gitRepoName.sync("../../");
+//let repoName = gitRepoName.sync("../../");
+require("dotenv").config();
 
 // Export the configuration object for the application
 module.exports = {
   app: {
     // Set the application name to the repository name
-    name: repoName,
+    // name: repo.name,
+    name: process.env.APP_NAME || "MyApp",
     // Define the API endpoint, defaulting to "/api" if the environment variable is not set
     apiEndpoint: process.env.API_URL ? `/${process.env.API_URL}` : "/api",
   },
   database: {
     // Define the database connection URL, defaulting to a MongoDB Atlas URL if not provided via an environment variable
-    url:
-      process.env.MONGODB_URI ||
-      `mongodb+srv://karenSchick:karen@cluster0.rnexktn.mongodb.net/?retryWrites=true`,
+    url: process.env.MONGODB_URI,
+
     // Set the database name, defaulting to "albumDataBase" if not provided
     name: process.env.MONGODB_NAME || "albumDataBase",
   },
   jwt: {
     // Set the JWT secret key, defaulting to "jwt-secret" if not provided
-    secret: process.env.JWT_SECRET || "jwt-secret",
+    secret: process.env.JWT_SECRET,
+
     // Define the lifespan of the JWT token
     tokenLife: "7d",
   },
